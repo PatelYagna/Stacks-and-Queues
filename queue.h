ca085f01de33d6d03aa3a4a9e44046914b10360d
PatelYagna <86132344+PatelYagna@users.noreply.github.com>
2023-10-30 22:23:52 -0500

    Update queue.h

--- queue.h
+++ queue.h
@@ -1,16 +1,54 @@
+#include <iostream>
 #include <queue>
+#include <limits>
 
 using namespace std;
 
 queue<int> tqueue;
 
-void insert(int number){
+void displayTheQueue() {
+	for (int i = 0; i < tqueue.size(); i++){
+		cout << tqueue.front() << " ";
+		tqueue.push(tqueue.front());
+		tqueue.pop();
+  }
+	cout << endl;
+}
+
+void insertNumberInBackOfQueue(int& number){
 	tqueue.push(number);
 }
 
-void display() {
-  while (!tqueue.empty()) {
-	// Print the front element of the queue.
-	cout << tqueue.front() << " ";
-  }
+void removeFrontNumberInQueue() {
+	if (!tqueue.empty()) {
+		int item = tqueue.front();
+		cout << "Removed: " << item << endl;
+		tqueue.pop();
+	}
+	else {
+		cout << "Queue is Empty." << endl;
+	}
+}
+
+void viewFrontNumberInQueue(){
+	if (!tqueue.empty()) {
+		int item = tqueue.front();
+		cout << "Front Number: " << item << endl;
+	}
+	else {
+		cout << "Queue is Empty." << endl;
+	}
+}
+
+void isQueueEmpty(){
+	if (tqueue.empty()){
+		cout << "Queue is Empty" << endl;
+	}
+	else{
+		cout << "Queue is NOT Empty" << endl;
+	}
+}
+
+void sizeOfQueue(){
+	cout << "Size: " << tqueue.size() << endl;
 }
