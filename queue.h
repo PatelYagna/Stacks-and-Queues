4263c9e8e7287342bdfd1ddbac7e65d237597063
PatelYagna <86132344+PatelYagna@users.noreply.github.com>
2023-11-14 09:40:19 -0600

    Update queue.h

--- queue.h
+++ queue.h
@@ -4,108 +4,117 @@
 
 using namespace std;
 
+// Node structure for the linked list used in the queue
 template <typename item_Type>
 struct Node { 
-	item_Type data; 
-	Node* next; 
+	item_Type data;  // Data of the node
+	Node* next;      // Pointer to the next node
 };
 
+// Queue class template
 template <typename item_Type>
 class queue {
-	private:
-		Node<item_Type>* head;
-		Node<item_Type>* tail;
-		int num_Items;
+private:
+	Node<item_Type>* head;  // Pointer to the front of the queue
+	Node<item_Type>* tail;  // Pointer to the rear of the queue
+	int num_Items;         // Number of items in the queue
 
-	public:
-		queue() {
-			head = nullptr;
-			tail = nullptr;
-			num_Items = 0;
-		};
+public:
+	// Constructor to initialize the queue
+	queue() {
+		head = nullptr;
+		tail = nullptr;
+		num_Items = 0;
+	};
 
+	// Destructor to deallocate memory used by the queue
+	~queue() {
+		while (head != nullptr){
+			Node<item_Type>* tempNode = head;
+			head = head->next;
+			delete tempNode;
+		}
+	};
 
-		~queue() {
-			while (head != nullptr){
-				Node<item_Type>* tempNode = head;
-				head = head->next;
-				delete tempNode;
+	// Enqueue an item into the queue
+	item_Type enQueue(){
+		item_Type item = ask();
+		Node<item_Type>* tempNode = new Node<item_Type>;
+		tempNode->data = item;
+		tempNode->next = nullptr;
+		if (isEmpty()){ 
+			head = tempNode; 
+		}
+		else { 
+			tail->next = tempNode; 
+		}
+		tail = tempNode;
+		num_Items++;
+		return tail->data;
+	};
+
+	// Dequeue an item from the queue
+	item_Type deQueue(){
+		if (isEmpty()) {
+			cout << "Empty List!" << endl;
+			return "Nothing Removed";
+		}
+		else {
+			Node<item_Type>* tempNode = head;
+			item_Type removedData = head->data;
+			head = head->next;
+			delete tempNode;
+			if (head == nullptr){
+				tail = nullptr;
 			}
-		};
+			num_Items--;
+			cout << "Removed: " << removedData << endl;
+			return removedData;
+		}
+	};
 
-		item_Type enQueue(){
-			item_Type item = ask();
-			Node<item_Type>* tempNode = new Node<item_Type>;
-			tempNode->data = item;
-			tempNode->next = nullptr;
-			if (isEmpty()){ 
-				head = tempNode; 
-			}
-			else { 
-				tail->next = tempNode; 
-			}
-			tail = tempNode;
-			num_Items++;
-			return tail->data;
-		};
+	// View the front item in the queue
+	item_Type viewFront(){
+		if (isEmpty()){
+			return "Empty List!";
+		}
+		else {
+			cout << "Front Item: " ;
+			return head->data;
+		}
+	};
 
+	// Check if the queue is empty
+	bool isEmpty(){
+		return num_Items == 0;
+	};
 
-		item_Type deQueue(){
-			if (isEmpty()) {
-				cout << "Empty List!" << endl;
-				return "Nothing Removed";
-			}
-			else {
-				Node<item_Type>* tempNode = head;
-				item_Type removedData = head->data;
-				head = head->next;
-				delete tempNode;
-				if (head == nullptr){
-					tail = nullptr;
-				}
-				num_Items--;
-				cout << "Removed: " << removedData << endl;
-				return removedData;
-			}
-		};
+	// Get the size of the queue
+	int queueSize(){
+		return num_Items;
+	};
 
-		item_Type viewFront(){
-			if (isEmpty()){
-				return "Empty List!";
+	// Display the items in the queue
+	void display(){
+		if (isEmpty()) {
+			cout << "Empty List!" << endl;
+		}
+		else {
+			Node<item_Type>* tempNode = head;
+			while (tempNode != nullptr) {
+				cout << tempNode->data << " ";
+				tempNode = tempNode->next;
 			}
-			else {
-				cout << "Front Item: " ;
-				return head->data;
-			}
-		};
+			cout << endl;
+		}
+	};
 
-		bool isEmpty(){
-			return num_Items == 0;
-		};
-
-		int queueSize(){
-			return num_Items;
-		};
-
-		void display(){
-			if (isEmpty()) {
-				cout << "Empty List!" << endl;
-			}
-			else {
-				Node<item_Type>* tempNode = head;
-				while (tempNode != nullptr) {
-					cout << tempNode->data << " ";
-					tempNode = tempNode->next;
-				}
-				cout << endl;
-			}
-		};
-
-		item_Type ask(){
-			item_Type input;
-			cout << "Enter Number: ";
-			cin >> input;
-			cin.ignore(numeric_limits<std::streamsize>::max(), '\n');
-			return input;
-		};
+	// Ask the user to enter a number and return it
+	item_Type ask(){
+		item_Type input;
+		cout << "Enter Number: ";
+		cin >> input;
+		cin.ignore(numeric_limits<std::streamsize>::max(), '\n');
+		return input;
+	};
 };
